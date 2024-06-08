08d13566f2487fd5af9e4392706b6d801b91a0bf
Max Hassen <max27183141@gmail.com>
2024-06-08 10:38:16 -0700

    set up typescript

---
+++ src/options.ts
@@ -0,0 +1,14 @@
+// this will be a template for an options object
+// when the user creates a new auth strategy
+// this will be used to configure the strategy
+// with options such as the ability to automate hashing of passwords, rate limiting, etc.
+
+interface Options {
+    verify: (username: string, password: string) => boolean;
+    hash?: number;
+    rateLimit?: number; // number of requests allowed in a certain time frame
+    rateLimitTime?: number; // in seconds, defaults to 1 minute
+    saltRounds?: number; // number of rounds to hash the password
+}
+
+export default Options;
