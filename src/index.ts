20751a9683fe196d3f764a0977aab4f23a0e6165
Max Hassen <max27183141@gmail.com>
2024-06-11 19:00:32 -0700

    added skeleton for strategy class

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,10 @@ class Strategy {
     private options: Options;
     constructor(options: Options) {
         this.options = {
+            // maybe include a customizable user object here
+            // also a customizable user database, such as a MongoDB connection
+            // otherwise, the user will have to implement their own database connection,
+            // look into express-rate-limit 
             verify: (username: string, password: string) => {
                 return false;
             },
@@ -13,8 +17,17 @@ class Strategy {
             blockIP: true,
             saltRounds: 10
         }
-    }
+    } 
     public setOptions(options: Options) {
         Object.assign(this.options, options);
     }
+    public verify(username: string, password: string, ip: string) {
+        if (this.options.blockIP) {
+            // check if the IP address is blocked
+            // if it is, return an error
+        }
+    }
 }
+
+
+export default Strategy;
