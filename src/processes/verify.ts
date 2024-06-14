861b12e5bb4f6dfa01929e0af3dbc2efedfd5420
Max Hassen <max27183141@gmail.com>
2024-06-13 18:00:11 -0700

    fixed eslint config stuff

---
+++ src/processes/verify.ts
@@ -0,0 +1,10 @@
+// middleware that verifies the user credentials and returns a boolean value
+import express, { Request, Response, NextFunction } from 'express';
+
+
+
+const Verify = (req : Request, res : Response, next: NextFunction) =>{
+    
+};
+
+export default Verify;
